edf015414099ea8cab9dea8121bada95f82e060d
michael <scottrodriguez2k@gmail.com>
2024-09-21 10:51:43 -0600

    feat: modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,25 +5,28 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
-  ModalFooter,
-  Button,
   Grid,
   GridItem,
   Heading,
   Text,
   Divider,
   Flex,
+  Image,
 } from '@chakra-ui/react'
 import { useAppDispatch, useAppSelector } from '../hooks'
 import { clearActiveItem, toggleModal } from '../store'
 
+const defaultImage = '/assets/default-image.svg'
 export const ModalCustom = () => {
   const { isOpen } = useAppSelector((state) => state.ui)
   const dispatch = useAppDispatch()
+  const { activeItem } = useAppSelector((state) => state.ui)
+
   const onClose = () => {
     dispatch(clearActiveItem())
     dispatch(toggleModal())
   }
+
   return (
     <Modal onClose={onClose} isOpen={isOpen} isCentered size={'4xl'}>
       <ModalOverlay />
@@ -31,59 +34,67 @@ export const ModalCustom = () => {
         <ModalHeader></ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-            <GridItem my="0.2rem">
-              <Button colorScheme="green">Button 1</Button>
+          <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+            <GridItem my="0.1rem" colSpan={1}>
+              <Image
+                src={activeItem?.selfie ?? defaultImage}
+                objectFit="cover" // Esto asegura que la imagen cubra todo el área disponible
+                width="100%" // Asegura que la imagen tome todo el ancho disponible
+                height="100%" // Asegura que la imagen tome todo el alto disponible
+              />
             </GridItem>
-            <GridItem >
-              <Heading my="0.7rem" size={'lg'}>Juan Carlos Perez Lopez</Heading>
+            <GridItem colSpan={2}>
+              <Heading my="0.7rem" size={'lg'}>
+                {activeItem?.firstName} {activeItem?.lastName}
+              </Heading>
               <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-                <GridItem my="0.2rem">
+                <GridItem my="0.1rem">
                   <Text>Correo electrónico:</Text>
-                  <Text color="gray.500">examel</Text>
+                  <Text color="gray.500">{activeItem?.email}</Text>
                 </GridItem>
-                <GridItem my="0.2rem">
+                <GridItem my="0.1rem">
                   <Text>Departamento:</Text>
-                  <Text color="gray.500">San Salvador</Text>
+                  <Text color="gray.500">{activeItem?.region}</Text>
                 </GridItem>
-                <GridItem my="0.2rem">
-                  <Text>Correo electrónico:</Text>
-                  <Text color="gray.500">examel</Text>
+                <GridItem my="0.1rem">
+                  <Text>Número de teléfono:</Text>
+                  <Text color="gray.500">{activeItem?.phoneNumber}</Text>
                 </GridItem>
-                <GridItem my="0.2rem">
-                  <Text>Departamento:</Text>
-                  <Text color="gray.500">San Salvador</Text>
+                <GridItem my="0.1rem">
+                  <Text>Municipio:</Text>
+                  <Text color="gray.500">{activeItem?.city}</Text>
                 </GridItem>
-                <GridItem my="0.2rem">
-                  <Text>Correo electrónico:</Text>
-                  <Text color="gray.500">examel</Text>
+                <GridItem my="0.1rem">
+                  <Text>Tipo de documento:</Text>
+                  <Text color="gray.500">{activeItem?.identificationType}</Text>
                 </GridItem>
-                <GridItem my="0.2rem">
-                  <Text>Departamento:</Text>
-                  <Text color="gray.500">San Salvador</Text>
+                <GridItem my="0.1rem">
+                  <Text>Dirección:</Text>
+                  <Text color="gray.500">{activeItem?.address}</Text>
                 </GridItem>
-                <GridItem my="0.2rem">
-                  <Text>Correo electrónico:</Text>
-                  <Text color="gray.500">examel</Text>
+                <GridItem my="0.1rem">
+                  <Text>Número de documento:</Text>
+                  <Text color="gray.500">
+                    {activeItem?.identificationNumber}
+                  </Text>
                 </GridItem>
-                <GridItem my="0.2rem">
-                  <Text>Departamento:</Text>
-                  <Text color="gray.500">San Salvador</Text>
+                <GridItem my="0.1rem">
+                  <Text>Ingresos mensuales:</Text>
+                  <Text color="gray.500">${activeItem?.monthlyIncome}</Text>
                 </GridItem>
               </Grid>
             </GridItem>
           </Grid>
           <Divider my="1rem" />
-          <Heading size={'md'} color={'gray.500'}>Documento de identidad</Heading>
-          <Flex gap={5}>
-            <Text>Documento de identidad:</Text>
-            <Text color="gray.500">examel</Text>
+          <Heading size={'md'} color={'gray.500'}>
+            Documento de identidad
+          </Heading>
+          <Flex gap={5} my={'1.5rem'}>
+            <Image src={activeItem?.identificationFront ?? defaultImage} />
+            <Image src={activeItem?.identificationBack ?? defaultImage} />
           </Flex>
-
         </ModalBody>
-        <ModalFooter>
-          <Button onClick={onClose}>Close</Button>
-        </ModalFooter>
+       
       </ModalContent>
     </Modal>
   )
