daa20236a2d0ffaf031ff82b83678ef2947c18d3
michael <scottrodriguez2k@gmail.com>
2024-09-22 14:37:05 -0600

    chore: reconocimiento de rostro

--- src/components/CameraCapture.tsx
+++ src/components/CameraCapture.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { Button, Box, Image, VStack, Flex, Text } from '@chakra-ui/react'
 import FileUpload from './FileUpload'
+import * as faceapi from 'face-api.js'
 
 interface Props {
   handlerPhoto: (photo: File) => void
@@ -14,66 +15,38 @@ const CameraCapture: React.FC<Props> = ({ handlerPhoto }) => {
   const [hasCamera, setHasCamera] = useState<boolean>(false)
 
   useEffect(() => {
-    const checkCameraAvailability = async () => {
-      try {
-        const devices = await navigator.mediaDevices.enumerateDevices()
-        const hasVideoInput = devices.some(
-          (device) => device.kind === 'videoinput',
-        )
-        setHasCamera(hasVideoInput)
-
-        if (hasVideoInput) {
-          const mediaStream = await navigator.mediaDevices.getUserMedia({
-            video: true,
-          })
-          setStream(mediaStream)
-          if (videoRef.current) {
-            videoRef.current.srcObject = mediaStream
-          }
-        }
-      } catch (error) {
-        console.error('Error checking camera availability:', error)
-      }
-    }
-
     checkCameraAvailability()
+
+    videoRef &&
+      loadModels().then(() => {
+        console.log('Models loaded')
+        faceMyDetect()
+      })
 
     return () => {
       stream?.getTracks().forEach((track) => track.stop())
     }
   }, [])
 
-  const takePhoto = () => {
-    const canvas = canvasRef.current
-    const video = videoRef.current
-    if (canvas && video) {
-      canvas.width = video.videoWidth
-      canvas.height = video.videoHeight
-      const context = canvas.getContext('2d')
-      if (context) {
-        context.drawImage(video, 0, 0)
-        const dataUrl = canvas.toDataURL('image/png')
-        setPhoto(dataUrl)
+  const checkCameraAvailability = async () => {
+    try {
+      const devices = await navigator.mediaDevices.enumerateDevices()
+      const hasVideoInput = devices.some(
+        (device) => device.kind === 'videoinput',
+      )
+      setHasCamera(hasVideoInput)
 
-        const dataUrlParts = dataUrl.split(',')
-        const mimeString = dataUrlParts[0].split(':')[1].split(';')[0]
-        const byteString = atob(dataUrlParts[1])
-        const arrayBuffer = new ArrayBuffer(byteString.length)
-        const uint8Array = new Uint8Array(arrayBuffer)
-
-        for (let i = 0; i < byteString.length; i++) {
-          uint8Array[i] = byteString.charCodeAt(i)
+      if (hasVideoInput) {
+        const mediaStream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        })
+        setStream(mediaStream)
+        if (videoRef.current) {
+          videoRef.current.srcObject = mediaStream
         }
-
-        const blob = new Blob([arrayBuffer], { type: mimeString })
-        const file = new File([blob], 'photo.png', { type: mimeString })
-
-        handlerPhoto(file)
-      } else {
-        console.error('Failed to get canvas context')
       }
-    } else {
-      console.error('Canvas or video element not found')
+    } catch (error) {
+      console.error('Error checking camera availability:', error)
     }
   }
 
@@ -91,18 +64,113 @@ const CameraCapture: React.FC<Props> = ({ handlerPhoto }) => {
     }
   }
 
+  const loadModels = () => {
+    return Promise.all([
+      // THIS FOR FACE DETECT AND LOAD FROM YOU PUBLIC/MODELS DIRECTORY
+      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+      faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    ]).then(() => {
+      faceMyDetect()
+    })
+  }
+
+  const dataUrlToFile = (dataUrl: string, filename: string): File => {
+    const dataUrlParts = dataUrl.split(',')
+    const mimeString = dataUrlParts[0].split(':')[1].split(';')[0]
+    const byteString = atob(dataUrlParts[1])
+    const arrayBuffer = new ArrayBuffer(byteString.length)
+    const uint8Array = new Uint8Array(arrayBuffer)
+
+    for (let i = 0; i < byteString.length; i++) {
+      uint8Array[i] = byteString.charCodeAt(i)
+    }
+
+    const blob = new Blob([arrayBuffer], { type: mimeString })
+    return new File([blob], filename, { type: mimeString })
+  }
+
+  const takePhoto = () => {
+    const canvas = canvasRef.current
+    if (canvas && videoRef.current) {
+      canvas.width = videoRef.current.videoWidth
+      canvas.height = videoRef.current.videoHeight
+
+      const dataUrl = getDataUrlFromCanvas(canvas)
+      if (dataUrl) {
+        setPhoto(dataUrl)
+        const file = dataUrlToFile(dataUrl, 'photo.png')
+        handlerPhoto(file)
+      } else {
+        console.error('Failed to get data URL from canvas')
+      }
+    } else {
+      console.error('Canvas or video element not found')
+    }
+  }
+
+  const getDataUrlFromCanvas = (canvas: HTMLCanvasElement): string | null => {
+    const context = canvas.getContext('2d')
+    if (context) {
+      context.drawImage(videoRef.current!, 0, 0)
+      return canvas.toDataURL('image/png')
+    }
+    console.error('Failed to get canvas context')
+    return null
+  }
+  const faceMyDetect = () => {
+    setInterval(async () => {
+      if (!videoRef.current || !canvasRef.current) return
+
+      const detections = await faceapi
+        .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions()
+
+      canvasRef.current.innerHTML += faceapi.createCanvasFromMedia(
+        videoRef.current,
+      )
+      faceapi.matchDimensions(canvasRef.current, {
+        width: 940,
+        height: 650,
+      })
+
+      const resized = faceapi.resizeResults(detections, {
+        width: 940,
+        height: 650,
+      })
+
+      faceapi.draw.drawDetections(canvasRef.current, resized)
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resized)
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resized)
+    }, 1000)
+  }
+
+ 
+
   return (
     <Flex direction="column" align="center" justify="center" p={4}>
       {hasCamera ? (
         <VStack spacing={4} align="center">
-          <Box boxSize="sm" borderRadius="md" overflow="hidden" m={2}>
+          <Box boxSize="sm" borderRadius="md" overflow="hidden" m={2} position="relative">
             <video
               ref={videoRef}
               autoPlay
-              style={{ width: '25rem', height: '25rem' }}
+              style={{ width: '100%', height: '100%' }}
             ></video>
+            <canvas
+              ref={canvasRef}
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '25rem',
+                height: '25rem',
+                zIndex: 1,
+              }}
+            />
           </Box>
-          <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
           {photo && (
             <Image
               src={photo}
@@ -141,3 +209,50 @@ const CameraCapture: React.FC<Props> = ({ handlerPhoto }) => {
 }
 
 export default CameraCapture
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // const takePhoto = () => {
+  //   const canvas = canvasRef.current
+  //   const video = videoRef.current
+  //   if (canvas && video) {
+  //     canvas.width = video.videoWidth
+  //     canvas.height = video.videoHeight
+  //     const context = canvas.getContext('2d')
+  //     if (context) {
+  //       context.drawImage(video, 0, 0)
+  //       const dataUrl = canvas.toDataURL('image/png')
+  //       setPhoto(dataUrl)
+
+  //       const dataUrlParts = dataUrl.split(',')
+  //       const mimeString = dataUrlParts[0].split(':')[1].split(';')[0]
+  //       const byteString = atob(dataUrlParts[1])
+  //       const arrayBuffer = new ArrayBuffer(byteString.length)
+  //       const uint8Array = new Uint8Array(arrayBuffer)
+
+  //       for (let i = 0; i < byteString.length; i++) {
+  //         uint8Array[i] = byteString.charCodeAt(i)
+  //       }
+
+  //       const blob = new Blob([arrayBuffer], { type: mimeString })
+  //       const file = new File([blob], 'photo.png', { type: mimeString })
+
+  //       handlerPhoto(file)
+  //     } else {
+  //       console.error('Failed to get canvas context')
+  //     }
+  //   } else {
+  //     console.error('Canvas or video element not found')
+  //   }
+  // }
